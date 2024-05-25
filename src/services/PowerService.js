1821a19b708c7f53519e2dd848de78597e179116
whatsgoodg <rlwjddl1596@naver.com>
2024-05-25 17:28:36 +0900

    added current power consumption rest api

--- src/services/PowerService.js
+++ src/services/PowerService.js
@@ -1,20 +1,17 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/power";
+const API_URL = "http://localhost:8080/test-power";
 
-class PowerService {
-  static async fetchPower() {
-    try {
-      const response = await axios.get(API_URL);
-      return response.data; // 데이터 반환
-    } catch (error) {
-      console.error("PowerService fetchPower Error:", error);
-      // 에러에 커스텀 메시지 또는 추가 정보를 포함할 수 있습니다.
-      throw new Error(
-        `An error occurred while fetching power data: ${error.message}`
-      );
-    }
+async function fetchPower() {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data; // 데이터 반환
+  } catch (error) {
+    console.error("PowerService fetchPower Error:", error);
+    // 에러에 커스텀 메시지 또는 추가 정보를 포함할 수 있습니다.
+    throw new Error(
+      `An error occurred while fetching power data: ${error.message}`
+    );
   }
 }
-
-export default new PowerService();
+export default fetchPower
